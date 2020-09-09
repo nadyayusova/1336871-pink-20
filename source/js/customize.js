5422756cb1c704d2cb21c9c990224ed5d296c681
nadyayusova <60035655+nadyayusova@users.noreply.github.com>
2020-09-09 17:47:05 +0300

    Делает приблизительную верстку mobile, tablet, desktop

---
+++ source/js/customize.js
@@ -0,0 +1,3 @@
+var toolsButtons = document.querySelectorAll(".user-photo__button");
+
+// обработчики на все кнопки - убирать/добавлять класс user-photo__tools-item--active на user-photo__tools-item
